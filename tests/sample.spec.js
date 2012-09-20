56ae7907b539e351cb6599eefc76cb3f371f620d
Codier <pendyaaa@gmail.com>
2012-09-20 07:54:53 -0500

    drop in unit test framework -- jasmine-node

---
+++ tests/sample.spec.js
@@ -0,0 +1,9 @@
+describe('A sample unit test', function() {
+  it('should pass', function() {
+	expect(1).toBe(1);
+  });
+
+	//   it('should fail', function() {
+	// expect(true).toBeFalsy();
+	//   });
+});
